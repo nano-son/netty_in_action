17cfe22646d70176e0e9ddf6924b68e3c5321e7a
nano <nano.son@kakaocorp.com>
2020-03-20 00:18:03 +0900

    그냥

---
+++ src/test/java/myTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+public class myTest {
+    @Test
+    public void tt() {
+        System.out.println("hello");
+    }
+
+    @Test
+    public void tt2() {
+        System.out.println("hello2");
+    }
+}
